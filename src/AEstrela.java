203d4307c2668629b773d388f7ce38bc675b3044
ACassimiro <cassimiroaellison@gmail.com>
2016-11-29 20:44:06 -0300

    Macaxeira

--- src/AEstrela.java
+++ src/AEstrela.java
@@ -5,8 +5,14 @@ public class AEstrela implements IAlgoritmoDeBusca {
 	@Override
 	public String search(Vertex noIni, Vertex noFin, Graph grafo) {
 		Vertex noAux = noIni;
+		Double distância = 0;
 
-		while(!noIni.getId().equals(noFin.getId())){
+		while(!noAux.getId().equals(noFin.getId())){
+
+			for (Vertex v : noAux.getEdges()) {
+				grafo.getEclidDist(v.getId(), v.)
+			}
+
 
 		}
 
